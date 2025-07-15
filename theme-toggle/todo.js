7d97b9e77862c955deb20d64918e0b6eaa4c6d68
suzzs <ghimiresujata333@gmail.com>
2025-07-15 19:22:24 +0545

    Simple To-Do

--- theme-toggle/todo.js
+++ theme-toggle/todo.js
@@ -1,9 +1,35 @@
-const input = document.getElementById("input");
+/*const input = document.getElementById("input");
 const button = document.getElementById("button");
-let list;
-button.addEventListener("click",()=>{
-list=input.innerHTML;
 
-console.log(input.innerHTML);
-});
-console.log(list);
+button.addEventListener("click", () => {
+  let value = input.value;
+  let li = document.createElement("li");
+  let checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+
+  li.appendChild(document.createTextNode(value + " " ));
+  li.appendChild(checkbox);
+
+  document.getElementById("list").appendChild(li);
+});*/
+
+
+
+const input =document.getElementById("input");
+const button  =document.getElementById("button");
+
+ button.addEventListener("click",()=>{
+let value = input.value;
+let div = document.createElement("li");
+let checkbox =document.createElement("input");
+checkbox.type = "checkbox";
+ div.appendChild(document.createTextNode(value + " "));
+div.appendChild(checkbox);
+document.getElementById("list").appendChild(div);
+
+
+
+ checkbox.addEventListener("change",()=>{
+document.getElementById("list").appendChild(div).remove();
+ });
+ });
